a097dd0c5dc6961b3f154e2915f4fc8b85d5c981
LiamOSullivan <lmosulli@tcd.ie>
2020-09-06 20:14:38 +0100

    add static version of CSO data

--- public/javascripts/dummy-load.js
+++ public/javascripts/dummy-load.js
@@ -1,4 +1,5 @@
-d3.csv('./data/static/population.csv')
-  .then(data => {
-    console.log('csv loaded with length ' + data.length)
-  })
+d3.csv('./data/static/CNA13.json')
+    // d3.csv('https://statbank.cso.ie/StatbankServices/StatbankServices.svc/jsonservice/responseinstance/CNA13')
+    .then(data => {
+        console.log('csv loaded with length ' + data.length)
+    })
