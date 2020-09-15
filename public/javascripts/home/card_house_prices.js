9f03b2650f4de62b93ed4af5aef18712a0c41de4
LiamOSullivan <lmosulli@tcd.ie>
2020-09-15 16:12:31 +0100

    style cards

--- public/javascripts/home/card_house_prices.js
+++ public/javascripts/home/card_house_prices.js
@@ -9,7 +9,7 @@ import { CardChartLine } from '../modules/CardChartLine.js'
 import { fetchJsonFromUrlAsync } from '../modules/bcd-async.js'
 import JSONstat from 'https://unpkg.com/jsonstat-toolkit@1.0.8/import.mjs'
 
-async function main (options) {
+async function main(options) {
   // HPM05: Market-based Household Purchases of Residential Dwellings by Type of Dwelling, Dwelling Status, Stamp Duty Event, RPPI Region, Month and Statistic
 
   // addSpinner('chart-' + chartDivIds[0], `<b>statbank.cso.ie</b> for table <b>${TABLE_CODE}</b>: <i>Annual Rate of Population Increase</i>`)
@@ -52,20 +52,20 @@ async function main (options) {
     { type: 'arrobj' },
     (d, i) => {
       if (d[dimensions[0]] === categoriesType[0] &&
-                d[dimensions[1]] === categoriesStatus[0] &&
-                d[dimensions[2]] === categoriesStamp[0] &&
-                (d[dimensions[3]] === 'Cork City' ||
-                    d[dimensions[3]] === 'Cork County'
-                ) &&
-                d[dimensions[5]] === categoriesStat[2] &&
-                hasCleanValue(d)) {
+        d[dimensions[1]] === categoriesStatus[0] &&
+        d[dimensions[2]] === categoriesStamp[0] &&
+        (d[dimensions[3]] === 'Cork City' ||
+          d[dimensions[3]] === 'Cork County'
+        ) &&
+        d[dimensions[5]] === categoriesStat[2] &&
+        hasCleanValue(d)) {
         d.date = parseYearMonth(d.Month)
         d.label = d.Month
         return d
       }
     })
 
-  console.log(housePricesTable)
+  // console.log(housePricesTable)
 
   const housePricesNested = d3.nest()
     .key(function (d) { return d.Month })
