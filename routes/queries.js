0861e092ee91499a36da29f10efe804071ff45b3
LiamOSullivan <lmosulli@tcd.ie>
2020-10-13 20:40:20 +0100

    create live travel query template

--- routes/queries.js
+++ routes/queries.js
@@ -10,13 +10,21 @@ router.get('/', function (req, res, next) {
   })
 })
 
-// router.get('/geodemos', function (req, res, next) {
-//   // alert('you clicked q1')
-//   res.render('queries/geodemos', {
-//     title: 'Query: Geodemographics',
-//     page: ''
-//   })
-// })
+router.get('/geodemos', function (req, res, next) {
+  // alert('you clicked q1')
+  res.render('queries/geodemos', {
+    title: 'Query: Geodemographics',
+    page: ''
+  })
+})
+
+router.get('/livetravel', function (req, res, next) {
+  // alert('you clicked q1')
+  res.render('queries/query_live_travel', {
+    title: 'Query: Live Travel',
+    page: ''
+  })
+})
 
 // router.get('/geodemosHM', function (req, res, next) {
 //   // alert('you clicked q1')
