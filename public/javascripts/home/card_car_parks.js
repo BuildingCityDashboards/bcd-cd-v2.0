5acf11f4f1bc6ca836ae5be615c19f103ddaf9ef
LiamOSullivan <lmosulli@tcd.ie>
2020-10-13 18:22:26 +0100

    minor changes to card

--- public/javascripts/home/card_car_parks.js
+++ public/javascripts/home/card_car_parks.js
@@ -3,8 +3,10 @@
  Car Parks card
 
  TODO:
- - getlatest fo all readings, not just json[0]
- - add animated update trend
+ - get latest of all readings, not just json[0]
+ - add animated update trend arrows
+ - blank display if data age exceeds limit
+ - add countdown timer to next refresh when data old/error occurs or otherwise indicate last read was successful
 ***/
 'use strict'
 
@@ -26,13 +28,23 @@ async function main (options) {
     // if (refreshCountdown > 0) refreshCountdown -= refreshInterval / 2
   }
 
-  //   let refreshTimer = setInterval(updateCountdown, refreshInterval)
-  const RETRY_INTERVAL = 30000
-  const REFRESH_INTERVAL = 60000 * 10
+  const RETRY_INTERVAL = 20000
+  const REFRESH_INTERVAL = 30000 * 1
   let refreshTimeout
+  let lastReadTime
+  const dataAge = 0
+
+  let prevSpacesTotalFree
+  // const indicatorUpSymbol = "<span class='up-arrow'>&#x25B2;</span>"
+  // const indicatorDownSymbol = "<span class='down-arrow'>&#x25BC;</span>"
+  // const indicatorRightSymbol = "<span class='right-arrow'>&#x25BA;</span>"
+  // let prevBikesAvailableDirection = indicatorRightSymbol // '▶'
+  // let prevStandsAvailableDirection = indicatorRightSymbol // '▶'
+  // let prevBikesTrendString = ''// '(no change)'
+  // let prevStandsTrendString = '' // '(no change)'
 
   async function fetchData () {
-    console.log('fetch data')
+    // console.log('fetch data')
     let csv
     clearTimeout(refreshTimeout)
     try {
@@ -40,23 +52,25 @@ async function main (options) {
       csv = await fetchCsvFromUrlAsyncTimeout(options.displayoptions.data.href, 500)
       const json = d3.csvParse(csv)
       if (json) {
-        console.log('data updated')
-        console.log('json')
-        console.log(json)
+        // console.log('data updated')
+        // console.log('json')
+        // console.log(json)
 
         const date = new Date(json[0].date)
-        const lastReadTime = date.getHours() + ':' + date.getMinutes()
+        lastReadTime = date.getHours() + ':' + date.getMinutes().toString().padStart(2, '0')
         // .getHour() + ':' + json[0].date.getHours() + getMinutes()
-        console.log(lastReadTime)
+        // console.log(lastReadTime)
 
-        const cardElement = document.getElementById('car-parks-card')
-        const subtitleElement = cardElement.querySelector('#subtitle')
-        subtitleElement.innerHTML = 'Latest reading ' + lastReadTime
         let spacesTotalFree = 0
         json.forEach((d) => {
           spacesTotalFree += +d.free_spaces
         })
-        console.log(spacesTotalFree)
+        // console.log(spacesTotalFree)
+
+        // update the DOM
+        const cardElement = document.getElementById('car-parks-card')
+        const subtitleElement = cardElement.querySelector('#subtitle')
+        subtitleElement.innerHTML = 'Latest reading ' + lastReadTime
 
         const leftElement = cardElement.querySelector('#card-left')
         leftElement.innerHTML = '<h1>' + json.length + '</h1>' +
