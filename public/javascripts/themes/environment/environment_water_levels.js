438b9f7e66e6d06f33dc30686a50dbac81494e60
LiamOSullivan <lmosulli@tcd.ie>
2020-10-16 17:17:16 +0100

    add water chart (blank)

--- public/javascripts/themes/environment/environment_water_levels.js
+++ public/javascripts/themes/environment/environment_water_levels.js
@@ -1,6 +1,6 @@
 import { ChartLinePopup } from '../../modules/bcd-chart-line-popup.js'
 import { MultiLineChart } from '../../modules/MultiLineChart.js'
-import { getDefaultMapOptions, getLatLng } from '../../modules/bcd-maps.js'
+import { getDefaultMapOptions, getCityLatLng } from '../../modules/bcd-maps.js'
 // import { toUnicode } from 'punycode'
 
 (async () => {
@@ -18,12 +18,12 @@ import { getDefaultMapOptions, getLatLng } from '../../modules/bcd-maps.js'
       dragging: !L.Browser.mobile,
       tap: !L.Browser.mobile
     })
-    waterLevelMap.setView(getLatLng(), 8)
+    waterLevelMap.setView(getCityLatLng(), 8)
     waterLevelMap.addLayer(waterLevelTiles)
     const waterLevelMapIcon = L.icon({
       iconUrl: './images/environment/water-15.svg',
       iconSize: [20, 20] // orig size
-    // iconAnchor: [iconAX, iconAY] //,
+      // iconAnchor: [iconAX, iconAY] //,
       // popupAnchor: [-3, -76]
     })
     const WaterLevelMarker = L.Marker.extend({
@@ -34,8 +34,8 @@ import { getDefaultMapOptions, getLatLng } from '../../modules/bcd-maps.js'
     })
 
     const waterLevelPopupOptons = {
-    // 'maxWidth': '500',
-    // 'className': 'leaflet-popup'
+      // 'maxWidth': '500',
+      // 'className': 'leaflet-popup'
     }
 
     const waterLevelSitesCluster = L.markerClusterGroup()
@@ -120,7 +120,7 @@ import { getDefaultMapOptions, getLatLng } from '../../modules/bcd-maps.js'
   }
 })()
 
-async function getSites (url) {
+async function getSites(url) {
   // need to be able to look up the static data using cosit as key
   // want an array of objects for dublin counters
   const siteData = await d3.json(url)
@@ -137,7 +137,7 @@ async function getSites (url) {
   return siteData
 }
 
-function getPopup (d_) {
+function getPopup(d_) {
   let str = ''
   if (!d_.id) {
     str += '<div class="leaflet-popup-error">' +
@@ -163,7 +163,7 @@ function getPopup (d_) {
   return str
 }
 
-async function getSiteReadings (d_) {
+async function getSiteReadings(d_) {
   // console.log(d_)
 
   const readings = await d3.json('../data/Environment/waterLevel_levels/sound_reading_' + d_.id + '.json')
@@ -235,7 +235,7 @@ const getWaterLevels = async () => {
   })
 }
 
-async function getPopupPlot (d_) {
+async function getPopupPlot(d_) {
   const divId = `waterLevel-site-${d_.id}`
   const data = await getSiteReadings(d_)
 
@@ -244,9 +244,9 @@ async function getPopupPlot (d_) {
   // console.log(data[0])
   if (isToday(data[0].date)) {
     document.getElementById(divId + '-subtitle').innerHTML =
-    data[0].date.toString().split(' ')[0] + ' ' +
-    data[1].date.toString().split(' ')[1] + ' ' +
-    data[2].date.toString().split(' ')[2]
+      data[0].date.toString().split(' ')[0] + ' ' +
+      data[1].date.toString().split(' ')[1] + ' ' +
+      data[2].date.toString().split(' ')[2]
     const options = {
       data: data,
       elementId: '' + divId + '-plot',
@@ -260,17 +260,17 @@ async function getPopupPlot (d_) {
   }
 
   const str = '<div class="popup-error">' +
-          '<div class="row ">' +
-          "We can't get the waterLevel monitoring data for this location right now, please try again later" +
-          '</div>' +
-          '</div>'
+    '<div class="row ">' +
+    "We can't get the waterLevel monitoring data for this location right now, please try again later" +
+    '</div>' +
+    '</div>'
   // return d3.select('#bike-spark-' + sid_)
   //   .html(str)
   document.getElementById(divId + '-plot').innerHTML = str
   return str
 }
 
-function activeBtn (e) {
+function activeBtn(e) {
   const btn = e
   $(btn).siblings().removeClass('active')
   $(btn).addClass('active')
