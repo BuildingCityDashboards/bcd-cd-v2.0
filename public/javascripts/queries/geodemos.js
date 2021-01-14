7bcb4198bb4cee8a219f9c77904c98c8eeaf6465
LiamOSullivan <lmosulli@tcd.ie>
2021-01-14 11:55:24 +0000

    add basic geodemos layers using new geojson SAs

--- public/javascripts/queries/geodemos.js
+++ public/javascripts/queries/geodemos.js
@@ -3,24 +3,22 @@
 **/
 
 import { getCityLatLng } from '../modules/bcd-maps.js'
+
 const minZoom = 10
 const maxZoom = 16
 const zoom = minZoom
 // tile layer with correct attribution
 const BASEMAP = 'https://{s}.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}.png'
 const ATTRIBUTION = '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, © <a href="https://carto.com/">CartoDB </a> contributors'
-const columnData = {}
-const punti_mappa = []
-const myTestArray = []
-const cov = 0
+
 const mapGeodemos = new L.Map('map-geodemos')
-const osm = new L.TileLayer(BASEMAP, {
+const osmLayer = new L.TileLayer(BASEMAP, {
   minZoom: minZoom,
   maxZoom: maxZoom,
   attribution: ATTRIBUTION
 })
 mapGeodemos.setView(getCityLatLng(), zoom)
-mapGeodemos.addLayer(osm)
+mapGeodemos.addLayer(osmLayer)
 
 L.control.locate({
   strings: {
@@ -42,7 +40,8 @@ const GEODEMOS_COLORWAY_CATEGORICAL = ['#7fc97f',
   '#bf5b17']
 const GEODEMOS_COLORWAY_CBSAFE = ['#d73027', '#f46d43', '#fdae61', '#fee090', '#abd9e9', '#74add1', '#4575b4']
 const GEODEMOS_COLORWAY = GEODEMOS_COLORWAY_CATEGORICAL
-// const gToLa =['Group1','Group2','Group3','Group4','Group5','Group6','Group7']
+
+// Used when an SA is null or otherwise falsey
 const naStyle = {
   fillColor: 'grey',
   weight: 1,
@@ -57,11 +56,8 @@ const mapLayers = getEmptyLayersArray(8)
 const traces = []
 const ntraces = []
 let layout = {}
-const tracesIndx = []
 let hmlayout = {}
-const columnNames = {}
 let columnNames2 = {}
-const tarry = []
 
 d3.csv('/data/geodemos/cork_zscores.csv')
   .then((zScores) => {
@@ -108,7 +104,7 @@ d3.csv('/data/geodemos/cork_zscores.csv')
       size: 17
 
     },
-      layout.showlegend = false
+    layout.showlegend = false
     layout.legend = Object.assign({}, ROW_CHART_LAYOUT.legend)
     layout.legend.xanchor = 'right'
 
@@ -132,7 +128,7 @@ d3.csv('/data/geodemos/cork_zscores.csv')
     layout.yaxis.title = Object.assign({}, ROW_CHART_LAYOUT.yaxis.title)
 
     layout.plot_bgcolor = '#293135',
-      layout.paper_bgcolor = '#293135'
+    layout.paper_bgcolor = '#293135'
 
     layout.yaxis.title = ''
     layout.margin = Object.assign({}, ROW_CHART_LAYOUT.margin)
@@ -149,107 +145,7 @@ d3.csv('/data/geodemos/cork_zscores.csv')
   }) // end then
 let lyt = {}
 
-function scatterHM() {
-  d3.csv('/data/geodemos/cork_zscores.csv')
-    .then((zScores) => {
-      columnNames2 = Object.keys(zScores[0])
-      columnNames2 = columnNames2.reverse()
-      columnNames2 = columnNames2.filter(e => e !== 'cluster')
-
-      zScores.forEach((row, i) => {
-        const ntrace = Object.assign({}, TRACES_DEFAULT)
-        ntrace.type = 'scatter'
-        ntrace.mode = 'markers+text'
-
-        ntrace.marker = Object.assign({}, TRACES_DEFAULT.marker)
-        ntrace.marker = {
-          color: getLayerColor(i),
-          size: 11
-
-        }
-
-        ntrace.x = columnNames2.map(name2 => {
-          return row[name2]
-        })
-
-        ntrace.y = columnNames2
-
-        ntraces.push(ntrace)
-        ntrace.hovertemplate = `%{x:.2f}<extra>Group No: ${i + 1}</extra>`
-      })
-
-      lyt = Object.assign({}, ROW_CHART_LAYOUT)
-      lyt.mode = 'scatter'
-      lyt.height = 500
-      // lyt.width = 300
-      lyt.plot_bgcolor = '#293135',
-        lyt.paper_bgcolor = '#293135'
-
-      lyt.title = Object.assign({}, ROW_CHART_LAYOUT.title)
-      lyt.title.text = 'Variables Value Distribution (z-scores)'
-      lyt.title.x = 0.51
-      lyt.title.y = 0.99
-      lyt.title.xanchor = 'center'
-      lyt.title.yanchor = 'top'
-      lyt.title.font = {
-        color: '#6fd1f6',
-        family: 'Roboto',
-        size: 17
-
-      },
-
-        lyt.legend = Object.assign({}, ROW_CHART_LAYOUT.legend)
-      lyt.legend.xanchor = 'right'
-      lyt.legend.y = 0.1
-      lyt.legend.traceorder = 'reversed'
-      lyt.xaxis = Object.assign({}, ROW_CHART_LAYOUT.xaxis)
-      lyt.xaxis.title = ''
-      lyt.xaxis.range = [-2, 2.9]
-      lyt.yaxis = Object.assign({}, ROW_CHART_LAYOUT.yaxis)
-      lyt.yaxis.tickfont = {
-        family: 'PT Sans',
-        size: 9,
-        color: '#6fd1f6'
-      }
-      lyt.xaxis.tickfont = {
-        family: 'PT Sans',
-        size: 10,
-        color: '#6fd1f6'
-      }
-
-      lyt.yaxis.titlefont = Object.assign({}, ROW_CHART_LAYOUT.yaxis.titlefont)
-      lyt.yaxis.titlefont.size = 16 // bug? need to call this
-      lyt.yaxis.title = Object.assign({}, ROW_CHART_LAYOUT.yaxis.title)
-
-      lyt.plot_bgcolor = '#293135',
-        lyt.paper_bgcolor = '#293135'
-
-      lyt.yaxis.title = ''
-      lyt.margin = Object.assign({}, ROW_CHART_LAYOUT.margin)
-
-      lyt.margin = {
-        l: 20,
-        r: 60, // annotations space
-        t: 40,
-        b: 0
-
-      }
-
-      Plotly.newPlot('chart-geodemos', [ntraces[0], ntraces[1], ntraces[2], ntraces[3], ntraces[4], ntraces[5], ntraces[6]], lyt, {
-        modeBar: {
-          orientation: 'v',
-          bgcolor: 'black',
-          color: null,
-          activecolor: null
-        }
-
-      })
-    }) // end then
-}
-// let soc_eco_val=0;
-// loadData()
-
-function loadData(file) {
+function loadGeojson (file) {
   d3.csv('/data/geodemos/cork_clusters_sa_cluster.csv')
     .then((data) => {
       const idClusterLookup = {}
@@ -260,7 +156,7 @@ function loadData(file) {
     })
 }
 
-async function loadSmallAreas() {
+async function loadSmallAreas () {
   const features = []
 
   const remoteURI = 'https://services1.arcgis.com/eNO7HHeQ3rUcBllm/arcgis/rest/services/Census2016_Theme5Table2_SA/FeatureServer/0/query?where=COUNTYNAME%20%3D%20\'CORK%20COUNTY\'&outFields=OBJECTID,GUID,COUNTY,COUNTYNAME,SMALL_AREA,Shape__Area,Shape__Length&outSR=4326&f=json'
@@ -269,30 +165,20 @@ async function loadSmallAreas() {
 
   const corkSAs = await d3.json(staticURI)
 
-  console.log(corkSAs.features)
-
   corkSAs.features.forEach(sa => {
     try {
-      console.log(sa)
       const groupNo = sa.properties.cork_clusters_Clusters
-      console.log(groupNo)
-      console.log(sa.properties.SMALL_AREA)
-      console.log(parseInt(groupNo) - 1)
       mapLayers[parseInt(groupNo) - 1].addData(sa)
     } catch (err) {
       // sa.properties.groupnumber = 'NA'
       // mapLayers[7].addData(sa)
       console.log(err)
     }
-    // console.log(layerNo)
   })
-
-  // AddLayersToMap()
 }
-
 loadSmallAreas()
 
-function getEmptyLayersArray(total) {
+function getEmptyLayersArray (total) {
   const layersArr = []
   for (let i = 0; i < total; i += 1) {
     layersArr.push(L.geoJSON(null, {
@@ -305,7 +191,7 @@ function getEmptyLayersArray(total) {
   return layersArr
 }
 
-function getLayerStyle(index) {
+function getLayerStyle (index) {
   return {
     fillColor: getLayerColor(index),
     weight: 0.3,
@@ -315,18 +201,14 @@ function getLayerStyle(index) {
     fillOpacity: 0.9
   }
 }
-function getLayerColor(index) {
+
+function getLayerColor (index) {
   return GEODEMOS_COLORWAY[index]
 }
 
-function updateGroupTxt(no) {
+function updateGroupTxt (no) {
   if (document.contains(document.getElementById('myhref'))) {
     document.getElementById('href').remove()
-  }
-
-  const dd = document.getElementById('desc')
-  if (no === 'all') {
-    no = 'all1'
   }
 
   d3.json('/data/geodemos/geodem-text-data.json').then(function (dublinRegionsJson) {
@@ -336,21 +218,8 @@ function updateGroupTxt(no) {
     d3.select('#group-text').text(dublinRegionsJson[0][no]).style('font-size', '15px')
   })
 }
-function getFColor(d) {
-  return d > 2.0 ? '#FFFFFF'
-    : d > 1.5 ? '#BFB6B3'
-      : d > 1.0 ? '#d99a1c'
-        : d > 1 ? '#989290'
-          : d == 1 ? '#746F6D'
 
-            : '#000000'
-}
-const ttt = []
-
-const value = 0
-const text = ''
-
-function onEachFeature(feature, layer) {
+function onEachFeature (feature, layer) {
   const customOptions =
   {
     maxWidth: '400',
@@ -400,7 +269,7 @@ d3.select('#group-buttons').selectAll('img').on('click', function () {
   }
 })
 
-function AddLayersToMap() {
+function AddLayersToMap () {
   mapLayers.forEach((l, k) => {
     if (!mapGeodemos.hasLayer(l)) {
       const mlay = mapLayers[k]
@@ -418,7 +287,7 @@ function AddLayersToMap() {
   })
 }
 
-function ResetImages(imgid) {
+function ResetImages (imgid) {
   const imgsrcarr = ['/images/icons/ui/Icon_eye_selected-all.svg',
     '/images/icons/ui/Icon_eye_selected-1.svg',
     '/images/icons/ui/Icon_eye_selected-2.svg',
@@ -453,7 +322,7 @@ function ResetImages(imgid) {
   selectedImg.src = imgsrcarr[imgid]
 }
 
-function addHorizrntalBars(value, text) {
+function addHorizrntalBars (value, text) {
   // alert(value + text)
   const GroupsArray = ['Group1', 'Group2', 'Group3', 'Group4', 'Group5', 'Group6', 'Group7']
   hmlayout = Object.assign({}, ROW_CHART_LAYOUT)
@@ -557,4 +426,102 @@ function addHorizrntalBars(value, text) {
       // Plotly.purge('chart-geodemos');
       Plotly.newPlot('chart-geodemos', data, hmlayout)
     })
+}
+
+function scatterHM () {
+  d3.csv('/data/geodemos/cork_zscores.csv')
+    .then((zScores) => {
+      columnNames2 = Object.keys(zScores[0])
+      columnNames2 = columnNames2.reverse()
+      columnNames2 = columnNames2.filter(e => e !== 'cluster')
+
+      zScores.forEach((row, i) => {
+        const ntrace = Object.assign({}, TRACES_DEFAULT)
+        ntrace.type = 'scatter'
+        ntrace.mode = 'markers+text'
+
+        ntrace.marker = Object.assign({}, TRACES_DEFAULT.marker)
+        ntrace.marker = {
+          color: getLayerColor(i),
+          size: 11
+
+        }
+
+        ntrace.x = columnNames2.map(name2 => {
+          return row[name2]
+        })
+
+        ntrace.y = columnNames2
+
+        ntraces.push(ntrace)
+        ntrace.hovertemplate = `%{x:.2f}<extra>Group No: ${i + 1}</extra>`
+      })
+
+      lyt = Object.assign({}, ROW_CHART_LAYOUT)
+      lyt.mode = 'scatter'
+      lyt.height = 500
+      // lyt.width = 300
+      lyt.plot_bgcolor = '#293135',
+      lyt.paper_bgcolor = '#293135'
+
+      lyt.title = Object.assign({}, ROW_CHART_LAYOUT.title)
+      lyt.title.text = 'Variables Value Distribution (z-scores)'
+      lyt.title.x = 0.51
+      lyt.title.y = 0.99
+      lyt.title.xanchor = 'center'
+      lyt.title.yanchor = 'top'
+      lyt.title.font = {
+        color: '#6fd1f6',
+        family: 'Roboto',
+        size: 17
+
+      },
+
+      lyt.legend = Object.assign({}, ROW_CHART_LAYOUT.legend)
+      lyt.legend.xanchor = 'right'
+      lyt.legend.y = 0.1
+      lyt.legend.traceorder = 'reversed'
+      lyt.xaxis = Object.assign({}, ROW_CHART_LAYOUT.xaxis)
+      lyt.xaxis.title = ''
+      lyt.xaxis.range = [-2, 2.9]
+      lyt.yaxis = Object.assign({}, ROW_CHART_LAYOUT.yaxis)
+      lyt.yaxis.tickfont = {
+        family: 'PT Sans',
+        size: 9,
+        color: '#6fd1f6'
+      }
+      lyt.xaxis.tickfont = {
+        family: 'PT Sans',
+        size: 10,
+        color: '#6fd1f6'
+      }
+
+      lyt.yaxis.titlefont = Object.assign({}, ROW_CHART_LAYOUT.yaxis.titlefont)
+      lyt.yaxis.titlefont.size = 16 // bug? need to call this
+      lyt.yaxis.title = Object.assign({}, ROW_CHART_LAYOUT.yaxis.title)
+
+      lyt.plot_bgcolor = '#293135',
+      lyt.paper_bgcolor = '#293135'
+
+      lyt.yaxis.title = ''
+      lyt.margin = Object.assign({}, ROW_CHART_LAYOUT.margin)
+
+      lyt.margin = {
+        l: 20,
+        r: 60, // annotations space
+        t: 40,
+        b: 0
+
+      }
+
+      Plotly.newPlot('chart-geodemos', [ntraces[0], ntraces[1], ntraces[2], ntraces[3], ntraces[4], ntraces[5], ntraces[6]], lyt, {
+        modeBar: {
+          orientation: 'v',
+          bgcolor: 'black',
+          color: null,
+          activecolor: null
+        }
+
+      })
+    }) // end then
 }
