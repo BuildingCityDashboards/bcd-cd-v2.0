f72842d50e50b81dfa7a57ee129f094c65932a3c
LiamOSullivan <lmosulli@tcd.ie>
2021-01-15 16:04:28 +0000

    style query accordion

--- public/javascripts/modules/accordion.js
+++ public/javascripts/modules/accordion.js
@@ -3,9 +3,7 @@ TODO: remove JQ and move to bcd-ui module | issue #124
 */
 
 const title = $('.accordion__heading')
-let question = $('.according__question')
-let content = $('.accordion__content')
-
+const content = $('.accordion__content')
 
 title.click(function () {
   $(this).toggleClass('accordion__heading--active')
@@ -15,6 +13,20 @@ title.click(function () {
   $(this).parent().siblings().children('.accordion__heading').removeClass('accordion__heading--active')
   $(this).parent().siblings().removeClass('accordion__item--active')
 
-
   return false
 })
+
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
