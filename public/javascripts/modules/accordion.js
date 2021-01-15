55374a77e560a07d1d97d733d01cba5ccdb43c0e
LiamOSullivan <lmosulli@tcd.ie>
2021-01-15 22:18:32 +0000

    simplify accordians, fix 124 partial

--- public/javascripts/modules/accordion.js
+++ public/javascripts/modules/accordion.js
@@ -2,31 +2,32 @@
 TODO: remove JQ and move to bcd-ui module | issue #124
 */
 
-const title = $('.accordion__heading')
-const content = $('.accordion__content')
+// const title = $('.accordion__heading')
+// // const content = $('.accordion__content')
 
-title.click(function () {
-  $(this).toggleClass('accordion__heading--active')
-  $(this).parent().toggleClass('accordion__item--active')
+// title.click(function () {
+//   // $(this).toggleClass('accordion__heading--active')
+//   $(this).parent().toggleClass('accordion__item--active')
 
-  // make other accordion__items hidden
-  $(this).parent().siblings().children('.accordion__heading').removeClass('accordion__heading--active')
-  $(this).parent().siblings().removeClass('accordion__item--active')
+//   // make other accordion__items hidden
+//   // $(this).parent().siblings().children('.accordion__heading').removeClass('accordion__heading--active')
+//   // $(this).parent().siblings().removeClass('accordion__item--active')
 
-  return false
-})
+//   return false
+// })
 
-// var acc = document.getElementsByClassName("accordion");
-// var i;
+var acc = document.getElementsByClassName('accordion')
+var i
 
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var panel = this.nextElementSibling;
-//     if (panel.style.display === "block") {
-//       panel.style.display = "none";
-//     } else {
-//       panel.style.display = "block";
-//     }
-//   });
-// }
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener('click', function () {
+    console.log('clicked' + this.id)
+    this.classList.toggle('accordion--active')
+    // var panel = this.nextElementSibling
+    // if (panel.style.display === 'block') {
+    //   panel.style.display = 'none';
+    // } else {
+    //   panel.style.display = 'block';
+    // }
+  })
+}
