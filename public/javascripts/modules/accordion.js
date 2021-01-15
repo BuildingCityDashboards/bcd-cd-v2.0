cd075c0c1c3355baa18ab408acab681aa49f8697
LiamOSullivan <lmosulli@tcd.ie>
2021-01-15 12:08:42 +0000

    add accordion for visualisations

---
+++ public/javascripts/modules/accordion.js
@@ -0,0 +1,20 @@
+/*
+TODO: remove JQ and move to bcd-ui module | issue #124
+*/
+
+const title = $('.accordion__heading')
+let question = $('.according__question')
+let content = $('.accordion__content')
+
+
+title.click(function () {
+  $(this).toggleClass('accordion__heading--active')
+  $(this).parent().toggleClass('accordion__item--active')
+
+  // make other accordion__items hidden
+  $(this).parent().siblings().children('.accordion__heading').removeClass('accordion__heading--active')
+  $(this).parent().siblings().removeClass('accordion__item--active')
+
+
+  return false
+})
