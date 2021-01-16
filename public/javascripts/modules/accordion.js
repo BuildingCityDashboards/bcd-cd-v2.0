e0cc9a328b005452e91616ad0f2115c3df9a9502
LiamOSullivan <lmosulli@tcd.ie>
2021-01-16 17:35:04 +0000

    fix for hiding map

--- public/javascripts/modules/accordion.js
+++ public/javascripts/modules/accordion.js
@@ -16,18 +16,12 @@ TODO: remove JQ and move to bcd-ui module | issue #124
 //   return false
 // })
 
-var acc = document.getElementsByClassName('accordion')
-var i
+const acc = document.getElementsByClassName('accordion')
+let i
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener('click', function () {
     console.log('clicked' + this.id)
     this.classList.toggle('accordion--active')
-    // var panel = this.nextElementSibling
-    // if (panel.style.display === 'block') {
-    //   panel.style.display = 'none';
-    // } else {
-    //   panel.style.display = 'block';
-    // }
   })
 }
