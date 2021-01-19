f10b82c341fe9b2b5cfd3a2a9f8297f78950c3de
LiamOSullivan <lmosulli@tcd.ie>
2021-01-19 15:04:43 +0000

    fix geodemos interactions

--- public/javascripts/modules/accordion.js
+++ public/javascripts/modules/accordion.js
@@ -5,6 +5,9 @@ let i
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener('click', function () {
-    this.classList.toggle('accordion--active')
+    console.log('click on' + this)
+    this.classList.toggle('open')
+    const content = this.nextElementSibling
+    content.classList.toggle('open')
   })
 }
