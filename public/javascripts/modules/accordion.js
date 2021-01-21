2848a877381fcd5287c8d1dae0d24a0223aeaf28
LiamOSullivan <lmosulli@tcd.ie>
2021-01-21 11:37:36 +0000

    styles at tablet

--- public/javascripts/modules/accordion.js
+++ public/javascripts/modules/accordion.js
@@ -5,7 +5,7 @@ let i
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener('click', function () {
-    console.log('click on' + this)
+    // console.log('click on' + this)
     this.classList.toggle('open')
     const content = this.nextElementSibling
     content.classList.toggle('open')
