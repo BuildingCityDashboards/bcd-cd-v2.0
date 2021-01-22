fef5cdd660736a90dca2bae409eb1cfeaffac8b6
LiamOSullivan <lmosulli@tcd.ie>
2021-01-22 11:47:07 +0000

    add basic heatmap code

--- public/javascripts/queries/geodemos/geodemos.js
+++ public/javascripts/queries/geodemos/geodemos.js
@@ -5,17 +5,103 @@
 import { getCityLatLng } from '../../modules/bcd-maps.js'
 import { getTraceDefaults, getRowChartLayout } from '../../modules/bcd-plotly-utils.js'
 
+const GEODEMOS_COLORWAY_CATEGORICAL = ['#7fc97f',
+  '#beaed4',
+  '#fdc086',
+  '#ffff99',
+  '#386cb0',
+  '#f0027f',
+  '#bf5b17']
+const GEODEMOS_COLORWAY_CBSAFE = ['#d73027',
+  '#f46d43',
+  '#fdae61',
+  '#fee090',
+  '#abd9e9',
+  '#74add1',
+  '#4575b4']
+
+const CHART_COLOR = '#ffffff'
+const CHART_FONT = {
+  family: 'PT Sans',
+  size: 16,
+  color: '#313131'
+}
+const CHART_TITLE_FONT = {
+  family: 'PT Sans',
+  size: 20,
+  color: '#313131'
+}
+const CHART_COLORWAY = ['#e7a4b6', '#cd7eaf', '#a262a9', '#6f4d96', '#3d3b72', '#182844']
+
+const ROW_CHART_LAYOUT = {
+  height: 400,
+  margin: {
+    l: 50,
+    r: 0,
+    b: 0,
+    t: 0
+  },
+  title: {
+    text: '',
+    font: {
+      family: null,
+      size: 20
+    },
+    visible: false,
+    xref: 'container',
+    x: 0.0,
+    xanchor: 'left',
+    yref: 'container',
+    y: 1.0,
+    yanchor: 'top'
+  },
+  xaxis: {
+    title: '',
+    titlefont: {
+      size: 16
+    },
+    visible: true,
+    type: null,
+    range: null,
+    fixedrange: true,
+    showticklabels: true,
+    nticks: null,
+    ticks: '',
+    automargin: true,
+    tickfont: {
+      family: null,
+      size: 12
+    }
+  },
+  yaxis: {
+    title: '',
+    titlefont: {
+      size: 16
+    },
+    visible: true,
+    type: null,
+    range: null,
+    fixedrange: true,
+    showticklabels: true,
+    nticks: null,
+    ticks: '',
+    automargin: true,
+    tickfont: {
+      family: null,
+      size: 12
+    }
+  },
+  paper_bgcolor: CHART_COLOR,
+  plot_bgcolor: CHART_COLOR,
+  colorway: CHART_COLORWAY,
+  font: CHART_FONT,
+  showlegend: false,
+  annotations: [],
+  hovermode: 'closest'
+
+}
+
 async function main () {
-  // add event listeners
-  const dd = document.getElementById('groups-dropdown')
-  dd.addEventListener('click', handleClick)
-  // dd.addEventListener('touchstart', handleClick)
-
-  function handleClick (e) {
-    console.log('toggle dropdown')
-    this.classList.toggle('show')
-  }
-
   // Add map
   const minZoom = 7
   const maxZoom = 16
@@ -58,8 +144,8 @@ async function main () {
   let mapLayers = await getEmptyLayersArray(8)
   mapLayers = await loadSmallAreas(mapLayers)
   addLayersToMap(mapLayers, mapGeodemos)
-
-  const chartTraces = await getChartTraces()
+  const zScores = await d3.csv('/data/geodemos/cork_zscores.csv')
+  const chartTraces = await getChartTraces(zScores)
   const chartLayout = await getChartLayout()
 
   Plotly.newPlot('chart-geodemos', chartTraces, chartLayout, {
@@ -71,9 +157,23 @@ async function main () {
     },
     responsive: true
   })
-  console.log('Plotly plot created')
 
   const descriptions = await d3.json('/data/geodemos/geodemos-group-descriptions.json')
+
+  const heatmapTraces = await getHeatmapTraces(zScores)
+  const heatmapLayout = await getHeatmapLayout()
+  console.log(heatmapTraces)
+
+  // Plotly.newPlot('geodemos-heatmap', heatmapTraces, heatmapLayout)
+
+  // add event listeners
+  const dd = document.getElementById('groups-dropdown')
+  dd.addEventListener('click', handleDropdownClick)
+  // dd.addEventListener('touchstart', handleClick)
+
+  function handleDropdownClick (e) {
+    this.classList.toggle('show')
+  }
 
   d3.select('#query-dropdown__content').selectAll('button').on('click', function () {
     const buttonData = $(this).attr('data') // TODO: remove jQ
@@ -131,7 +231,6 @@ async function getEmptyLayersArray (total) {
     layersArr.push(L.geoJSON(null, {
       style: getLayerStyle(i),
       onEachFeature: onEachFeature
-
     })
     )
   }
@@ -150,21 +249,6 @@ function getLayerStyle (index) {
 }
 
 function getLayerColor (index) {
-  const GEODEMOS_COLORWAY_CATEGORICAL = ['#7fc97f',
-    '#beaed4',
-    '#fdc086',
-    '#ffff99',
-    '#386cb0',
-    '#f0027f',
-    '#bf5b17']
-  const GEODEMOS_COLORWAY_CBSAFE = ['#d73027',
-    '#f46d43',
-    '#fdae61',
-    '#fee090',
-    '#abd9e9',
-    '#74add1',
-    '#4575b4']
-
   return GEODEMOS_COLORWAY_CBSAFE[index]
 }
 
@@ -199,71 +283,6 @@ function addLayersToMap (layers, map) {
       })
     }
   })
-}
-
-/* Chart functions */
-
-async function loadChartData (groupNames) {
-  d3.text('/data/geodemos/cork_zscores.csv')
-    .then((zScores) => {
-      const newCsv = zScores.split('\n').map(function (line) {
-        const columns = line.split(',') // get the columns
-        columns.splice(0, 1) // remove total column
-        return columns
-      }).join('\n')
-
-      const rows = newCsv.split('\n')
-
-      // get the first row as header
-      const header = rows.shift()
-
-      // const header = columnNames;
-      const numberOfColumns = header.split(',').length
-
-      // initialize 2D-array with a fixed size
-      const columnData = [...Array(numberOfColumns)].map(item => new Array())
-
-      for (let i = 0; i < rows.length; i++) {
-        const row = rows[i]
-        const rowData = row.split(',')
-
-        for (let j = 0; j < numberOfColumns; j++) {
-          columnData[j].push((rowData[j]))
-        }
-      }
-      const zArray = []
-      for (let r = 0; r < 7; r++) {
-        // alert(columnData[value][r])
-        zArray.push((columnData[value][r]))
-      }
-
-      const farr = []
-      // let tc ={}
-      for (let f = 0; f < zArray.length; f++) {
-        const tc = Object.assign({}, TRACES_DEFAULT)
-        tc.type = 'bar'
-        tc.orientation = 'h'
-        tc.x = parseFloat(zArray[f])
-        tc.y = groupNames[f]
-        // alert(JSON.stringify(tc))
-        farr.push(tc)
-      }
-
-      const data = [
-        {
-          x: zArray, // columnData,
-          y: groupNames,
-          hovertemplate: `${text}: %{x:.2f}<extra></extra>`,
-          type: 'bar',
-          orientation: 'h',
-          indxArr: [0, 1, 2, 3, 4, 5, 6],
-          marker: {
-            color: ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e', '#e6ab02', '#a6761d', '#666666']
-          }
-        }
-      ]
-      return data
-    })
 }
 
 function ResetImages (imgid) {
@@ -301,12 +320,9 @@ function ResetImages (imgid) {
   selectedImg.src = imgsrcarr[imgid]
 }
 
-/* Heatmap functions */
+/* Value chart functions */
 
-async function getChartTraces () {
-  const zScores = await d3.csv('/data/geodemos/cork_zscores.csv')
-  console.log('zscores')
-  console.log(JSON.stringify(zScores[0]))
+async function getChartTraces (zScores) {
   const traces = []
   let columnNames2 = {}
   columnNames2 = Object.keys(zScores[0])
@@ -339,13 +355,8 @@ async function getChartTraces () {
     visible: true // 'legendonly'
   }
 
-  console.log('TRACES_DEFAULT')
-  console.log(TRACES_DEFAULT)
-
   zScores.forEach((row, i) => {
     const trace = Object.assign({}, TRACES_DEFAULT)
-    console.log('trace')
-    console.log(trace)
     trace.type = 'scatter'
     trace.mode = 'markers+text'
 
@@ -364,7 +375,6 @@ async function getChartTraces () {
     traces.push(trace)
     trace.hovertemplate = `%{x:.2f}<extra>Group No: ${i + 1}</extra>`
   })
-  console.log('traces: ' + traces.length)
   return traces
 }
 
@@ -433,4 +443,235 @@ function updateGroupDescription (groupNo, contentText) {
 
   const content = document.getElementById('geodemos-group-description__content')
   content.innerHTML = contentText
+}
+
+/* Heatmap functions */
+
+function getHeatmapLayout () {
+  const heatmapLayout = Object.assign({}, ROW_CHART_LAYOUT)
+  heatmapLayout.height = 500
+  heatmapLayout.width = 560
+  // layout.barmode = 'group';
+  heatmapLayout.plot_bgcolor = '#293135'
+  heatmapLayout.paper_bgcolor = '#293135'
+
+  heatmapLayout.colorway = GEODEMOS_COLORWAY_CBSAFE
+  heatmapLayout.title = Object.assign({}, ROW_CHART_LAYOUT.title)
+  heatmapLayout.title.text = ''
+  heatmapLayout.title.x = 0.51
+  heatmapLayout.title.y = 0.99
+  heatmapLayout.title.xanchor = 'center'
+  heatmapLayout.title.yanchor = 'top'
+  heatmapLayout.title.font = {
+    color: '#6fd1f6',
+    family: 'Courier New, monospace',
+    size: 17
+  },
+  heatmapLayout.showlegend = false
+  heatmapLayout.legend = Object.assign({}, ROW_CHART_LAYOUT.legend)
+  heatmapLayout.legend.xanchor = 'right'
+  heatmapLayout.legend.y = 0.1
+  heatmapLayout.legend.traceorder = 'reversed'
+  heatmapLayout.xaxis = Object.assign({}, ROW_CHART_LAYOUT.xaxis)
+  heatmapLayout.xaxis.title = ''
+  heatmapLayout.yaxis = Object.assign({}, ROW_CHART_LAYOUT.yaxis)
+
+  heatmapLayout.yaxis.tickfont = {
+    family: 'PT Sans',
+    size: 10,
+    color: '#6fd1f6'
+  }
+  heatmapLayout.xaxis.tickfont = {
+    family: 'PT Sans',
+    size: 10,
+    color: '#6fd1f6'
+  }
+  heatmapLayout.tickfont = {
+    family: 'PT Sans',
+    size: 10,
+    color: '#6fd1f6'
+  }
+
+  heatmapLayout.yaxis.titlefont = Object.assign({}, ROW_CHART_LAYOUT.yaxis.titlefont)
+  heatmapLayout.yaxis.titlefont.size = 16 // bug? need to call this
+  heatmapLayout.yaxis.title = Object.assign({}, ROW_CHART_LAYOUT.yaxis.title)
+  heatmapLayout.yaxis.title = ''
+  heatmapLayout.margin = Object.assign({}, ROW_CHART_LAYOUT.margin)
+  heatmapLayout.margin = {
+    l: 20,
+    r: 40, // annotations space
+    t: 30,
+    b: 0
+
+  }
+  return heatmapLayout
+}
+
+function getHeatmapTraces (zScores) {
+  const GroupsArray = ['Group1', 'Group2', 'Group3', 'Group4', 'Group5', 'Group6', 'Group7']
+  const newCsv = zScores.split('\n').map(function (line) {
+    const columns = line.split(',') // get the columns
+    columns.splice(0, 1) // remove total column
+    return columns.reverse()
+  }).join('\n')
+
+  const rows = newCsv.split('\n')
+  // alert(rows)
+  // get the first row as header
+  const header = rows.shift()
+  // alert(header)
+  // const header = columnNames;
+  const numberOfColumns = header.split(',').length
+
+  // initialize 2D-array with a fixed size
+  const columnData = [...Array(numberOfColumns)].map(item => new Array())
+
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i]
+    const rowData = row.split(',')
+
+    // assuming that there's always the same
+    // number of columns in data rows as in header row
+    for (let j = 0; j < numberOfColumns; j++) {
+      columnData[j].push((rowData[j]))
+    }
+  }
+  const heatmapTraces = [
+    {
+
+      z: columnData,
+      x: GroupsArray,
+      y: header.split(','),
+
+      hovertemplate: 'z-score: %{z:.2f}<extra></extra>',
+      type: 'heatmap',
+      hoverinfo: 'z',
+      showscale: true,
+      fixedrange: true,
+      colorbar: {
+        // title: 'z-scores',
+
+        tickcolor: '#6fd1f6',
+        tickfont: {
+          color: '#6fd1f6',
+          size: 10
+        },
+
+        ticks: 'outside',
+        dtick: 0.25,
+        tickwidth: 2,
+        ticklen: 10,
+        showticklabels: true,
+        xpad: 35,
+
+        thickness: 40,
+        thicknessmode: 'pixels',
+        len: 0.9,
+        lenmode: 'fraction',
+        outlinewidth: 0
+
+      },
+      colorscale: [
+
+        // Let first 10% (0.1) of the values have color rgb(0, 0, 0)
+
+        [0, 'rgb(179,24,43)'],
+        [0.1, 'rgb(179,24,43)'],
+
+        // Let values between 10-20% of the min and max of z
+        // have color rgb(20, 20, 20)
+
+        [0.1, 'rgb(214,86,77)'],
+        [0.2, 'rgb(214,86,77)'],
+
+        // Values between 20-30% of the min and max of z
+        // have color rgb(40, 40, 40)
+
+        [0.2, 'rgb(244,165,130)'],
+        [0.3, 'rgb(244,165,130)'],
+
+        [0.3, 'rgb(253,219,199)'],
+        [0.4, 'rgb(253,219,199)'],
+
+        [0.4, 'rgb(247,247,247)'],
+        [0.5, 'rgb(247,247,247)'],
+
+        [0.5, 'rgb(209,229,240)'],
+        [0.6, 'rgb(209,229,240)'],
+
+        [0.6, 'rgb(146,197,222)'],
+        [0.7, 'rgb(146,197,222)'],
+
+        [0.7, 'rgb(67,147,195)'],
+        [0.8, 'rgb(67,147,195)'],
+
+        [0.8, 'rgb(33,102,172)'],
+        [1.0, 'rgb(33,102,172)']
+      ]
+    }
+  ]
+  return heatmapTraces
+}
+
+async function loadChartData (groupNames) {
+  d3.text('/data/geodemos/cork_zscores.csv')
+    .then((zScores) => {
+      const newCsv = zScores.split('\n').map(function (line) {
+        const columns = line.split(',') // get the columns
+        columns.splice(0, 1) // remove total column
+        return columns
+      }).join('\n')
+
+      const rows = newCsv.split('\n')
+
+      // get the first row as header
+      const header = rows.shift()
+
+      // const header = columnNames;
+      const numberOfColumns = header.split(',').length
+
+      // initialize 2D-array with a fixed size
+      const columnData = [...Array(numberOfColumns)].map(item => new Array())
+
+      for (let i = 0; i < rows.length; i++) {
+        const row = rows[i]
+        const rowData = row.split(',')
+
+        for (let j = 0; j < numberOfColumns; j++) {
+          columnData[j].push((rowData[j]))
+        }
+      }
+      const zArray = []
+      for (let r = 0; r < 7; r++) {
+        // alert(columnData[value][r])
+        zArray.push((columnData[value][r]))
+      }
+
+      const farr = []
+      // let tc ={}
+      for (let f = 0; f < zArray.length; f++) {
+        const tc = Object.assign({}, TRACES_DEFAULT)
+        tc.type = 'bar'
+        tc.orientation = 'h'
+        tc.x = parseFloat(zArray[f])
+        tc.y = groupNames[f]
+        // alert(JSON.stringify(tc))
+        farr.push(tc)
+      }
+
+      const data = [
+        {
+          x: zArray, // columnData,
+          y: groupNames,
+          hovertemplate: `${text}: %{x:.2f}<extra></extra>`,
+          type: 'bar',
+          orientation: 'h',
+          indxArr: [0, 1, 2, 3, 4, 5, 6],
+          marker: {
+            color: ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e', '#e6ab02', '#a6761d', '#666666']
+          }
+        }
+      ]
+      return data
+    })
 }
