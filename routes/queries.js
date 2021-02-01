3892664225fccd67793b710daa19c779a1050e59
LiamOSullivan <lmosulli@tcd.ie>
2021-02-01 15:35:07 +0000

    add card for live environment with routes

--- routes/queries.js
+++ routes/queries.js
@@ -11,27 +11,24 @@ router.get('/', function (req, res, next) {
 })
 
 router.get('/geodemos', function (req, res, next) {
-  // alert('you clicked q1')
   res.render('queries/geodemos', {
     title: 'Query: Geodemographics',
     page: ''
   })
 })
 
-router.get('/livetravel', function (req, res, next) {
-  // alert('you clicked q1')
+router.get('/live-travel', function (req, res, next) {
   res.render('queries/query_live_travel', {
     title: 'Query: Live Travel',
     page: ''
   })
 })
 
-// router.get('/geodemosHM', function (req, res, next) {
-//   // alert('you clicked q1')
-//   res.render('queries/geodemosHM', {
-//     title: 'Query: Geodemographics',
-//     page: ''
-//   })
-// })
+router.get('/live-environment', function (req, res, next) {
+  res.render('queries/query_live_travel', {
+    title: 'Query: Live Environment',
+    page: ''
+  })
+})
 
 module.exports = router
